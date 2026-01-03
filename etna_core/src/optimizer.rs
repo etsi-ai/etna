2d5dbb1e1aa998574a4caa9d733e8a63967cb4a9
Madhavi1108 <madhavi09307@gmail.com>
2026-01-03 15:31:04 +0530

    Restore Adam optimizer and OptimizerType enum

--- etna_core/src/optimizer.rs
+++ etna_core/src/optimizer.rs
@@ -1,4 +1,11 @@
-// # Optimizers (SGD, Adam, etc.)
+// Optimizers (SGD, Adam)
+
+/// Supported optimizer types
+#[derive(Clone, Copy)]
+pub enum OptimizerType {
+    SGD,
+    Adam,
+}
 
 /// Simple Stochastic Gradient Descent (SGD) optimizer
 pub struct SGD {
@@ -6,58 +13,89 @@ pub struct SGD {
 }
 
 impl SGD {
-    pub fn new(learning_rate: f32) -> Self {
-        SGD { learning_rate }
-    }
-
-    // Update weights and biases in-place
-    pub fn step(
-        &self,
-        weights: &mut Vec<Vec<f32>>,
-        weight_grads: &Vec<Vec<f32>>,
-        bias: &mut Vec<f32>,
-        bias_grads: &Vec<f32>,
-    ) {
-        for i in 0..weights.len() {
-            for j in 0..weights[0].len() {
-                weights[i][j] -= self.learning_rate * weight_grads[i][j];
-            }
-            bias[i] -= self.learning_rate * bias_grads[i];
+    pub fn new(lr: f32) -> Self {
+        Self {
+            learning_rate: lr,
         }
     }
 }
 
+/// Adam optimizer
+pub struct Adam {
+    lr: f32,
+    beta1: f32,
+    beta2: f32,
+    epsilon: f32,
+    t: usize,
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    // First and second moment estimates
+    m_w: Vec<Vec<f32>>,
+    v_w: Vec<Vec<f32>>,
+    m_b: Vec<f32>,
+    v_b: Vec<f32>,
+}
 
-    #[test]
-    fn sgd_updates_weight_correctly() {
-        let optimizer = SGD::new(0.1);
+impl Adam {
+    pub fn new(lr: f32, beta1: f32, beta2: f32, epsilon: f32) -> Self {
+        Self {
+            lr,
+            beta1,
+            beta2,
+            epsilon,
+            t: 0,
+            m_w: Vec::new(),
+            v_w: Vec::new(),
+            m_b: Vec::new(),
+            v_b: Vec::new(),
+        }
+    }
 
-        let mut weights = vec![vec![1.0]];
-        let weight_grads = vec![vec![0.1]];
+    pub fn step(
+        &mut self,
+        weights: &mut Vec<Vec<f32>>,
+        grad_w: &Vec<Vec<f32>>,
+        bias: &mut Vec<f32>,
+        grad_b: &Vec<f32>,
+    ) {
+        self.t += 1;
 
-        let mut bias = vec![0.0];
-        let bias_grads = vec![0.0];
+        // Lazy initialization (once per layer)
+        if self.m_w.is_empty() {
+            self.m_w = vec![vec![0.0; weights[0].len()]; weights.len()];
+            self.v_w = vec![vec![0.0; weights[0].len()]; weights.len()];
+            self.m_b = vec![0.0; bias.len()];
+            self.v_b = vec![0.0; bias.len()];
+        }
 
-        optimizer.step(
-            &mut weights,
-            &weight_grads,
-            &mut bias,
-            &bias_grads,
-        );
+        let t_f = self.t as f32;
 
-        // Floating point math is slightly dishonest, so use tolerance
-        let expected = 0.99;
-        let actual = weights[0][0];
+        // Update weights
+        for i in 0..weights.len() {
+            for j in 0..weights[0].len() {
+                self.m_w[i][j] =
+                    self.beta1 * self.m_w[i][j] + (1.0 - self.beta1) * grad_w[i][j];
+                self.v_w[i][j] =
+                    self.beta2 * self.v_w[i][j] + (1.0 - self.beta2) * grad_w[i][j].powi(2);
 
-        assert!(
-            (actual - expected).abs() < 1e-6,
-            "Expected weight to be {}, got {}",
-            expected,
-            actual
-        );
+                let m_hat = self.m_w[i][j] / (1.0 - self.beta1.powf(t_f));
+                let v_hat = self.v_w[i][j] / (1.0 - self.beta2.powf(t_f));
+
+                weights[i][j] -=
+                    self.lr * m_hat / (v_hat.sqrt() + self.epsilon);
+            }
+        }
+
+        // Update bias
+        for i in 0..bias.len() {
+            self.m_b[i] =
+                self.beta1 * self.m_b[i] + (1.0 - self.beta1) * grad_b[i];
+            self.v_b[i] =
+                self.beta2 * self.v_b[i] + (1.0 - self.beta2) * grad_b[i].powi(2);
+
+            let m_hat = self.m_b[i] / (1.0 - self.beta1.powf(t_f));
+            let v_hat = self.v_b[i] / (1.0 - self.beta2.powf(t_f));
+
+            bias[i] -= self.lr * m_hat / (v_hat.sqrt() + self.epsilon);
+        }
     }
 }
